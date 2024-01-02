7ec379c0beb429519f10977427cf2693aae36646
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2024-01-02 17:50:49 +0100

    Rozwiązanie zadania.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,12 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        Number number = new Number();
+        number.guessNumber(scanner);
     }
 }
---
+++ src/main/java/pl/javastart/task/Number.java
@@ -0,0 +1,37 @@
+package pl.javastart.task;
+
+import java.util.Scanner;
+
+public class Number {
+    private int number;
+
+    private boolean checkNumber() {
+        boolean result = false;
+        if (number < 100) {
+            showMessage("Podana liczba jest za mała");
+            result = false;
+        } else if (number >= 100 && number <= 200) {
+            if (number % 3 == 0) {
+                showMessage("Twoja liczba jest ok");
+            } else {
+                showMessage("Podana liczba nie jest podzielna przez 3");
+                result = false;
+            }
+        } else if (number > 200) {
+            showMessage("Podana liczba jest za duża");
+            result = false;
+        }
+        return !result;
+    }
+
+    public void guessNumber(Scanner scanner) {
+        do {
+            System.out.println("Podaj liczbę");
+            number = scanner.nextInt();
+        } while (checkNumber());
+    }
+
+    private void showMessage(String message) {
+        System.out.println(message);
+    }
+}
