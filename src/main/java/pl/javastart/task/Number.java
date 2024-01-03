e992262f52dbd0a149fbb8edc3926b2cc65de48e
AdamJaniczek <ajaniczek@itsystems.com.pl>
2024-01-03 10:44:23 +0100

    Poprawa kodu aplikacji

--- src/main/java/pl/javastart/task/Number.java
+++ src/main/java/pl/javastart/task/Number.java
@@ -3,36 +3,30 @@ package pl.javastart.task;
 import java.util.Scanner;
 
 public class Number {
-    private int number;
 
-    private boolean checkNumber() {
+    private boolean checkNumber(int number) {
         boolean result = false;
         if (number < 100) {
-            showMessage("Podana liczba jest za mała");
-            result = false;
-        } else if (number >= 100 && number <= 200) {
+            System.out.println("Podana liczba jest za mała");
+        } else if (number > 200) {
+            System.out.println("Podana liczba za za duża");
+
+        } else {
             if (number % 3 == 0) {
-                showMessage("Twoja liczba jest ok");
+                System.out.println("Twoja liczba jest ok");
                 result = true;
             } else {
-                showMessage("Podana liczba nie jest podzielna przez 3");
-                result = false;
+                System.out.println("Podana liczba nie jest podzielna przez 3");
             }
-        } else if (number > 200) {
-            showMessage("Podana liczba jest za duża");
-            result = false;
         }
         return !result;
     }
 
     public void guessNumber(Scanner scanner) {
+        int number;
         do {
             System.out.println("Podaj liczbę");
             number = scanner.nextInt();
-        } while (checkNumber());
-    }
-
-    private void showMessage(String message) {
-        System.out.println(message);
+        } while (checkNumber(number));
     }
 }
